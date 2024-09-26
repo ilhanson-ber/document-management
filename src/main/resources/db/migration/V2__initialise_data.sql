79b74044e9c72840f17ee756afd17d681cfa28be
Ilhan Son <ilhanson.ber@gmail.com>
2024-09-26 13:57:30 +0200

    add database migration (flyway)

--- src/main/resources/db/migration/V2__initialise_data.sql
+++ src/main/resources/db/migration/V2__initialise_data.sql
@@ -8,21 +8,22 @@ INSERT INTO author (first_name, last_name) VALUES
 ('Emily', 'Johnson');
 
 -- Insert documents
-INSERT INTO document (title, body) VALUES 
+INSERT INTO document (title, body) VALUES
 ('Document 1', 'Content of Document 1'),
 ('Document 2', 'Content of Document 2'),
 ('Document 3', 'Content of Document 3'),
 ('Document 4', 'Content of Document 4'),
-('Document 5', 'Content of Document 5');
+('Document 5', 'Content of Document 5'),
+('Document 6', 'Content of Document 6');
 
 -- Associate authors with documents in the author_document table
-INSERT INTO author_document (author_id, document_id) VALUES 
+INSERT INTO author_document (author_id, document_id) VALUES
 (1, 1), (1, 2), (1, 3),
 (2, 1), (2, 5),
 (3, 4), (3, 5);
 
 -- Set up document references in the document_reference table (document referencing other documents)
-INSERT INTO document_reference (document_id, reference_id) VALUES 
+INSERT INTO document_reference (document_id, reference_id) VALUES
 (1, 2), (1, 3), (1, 4),
 (3, 4),
 (4, 1), (4, 2),
